c87dfdf620500835c5151f05d039825416e87d83
mati-pc\matiullah <mati_ullah31@yahoo.com>
2019-01-05 00:30:32 +0500

    added admin panel tabs and menu for my plugin

--- wp-content/plugins/my-learning-plugin/templates/admin.php
+++ wp-content/plugins/my-learning-plugin/templates/admin.php
@@ -1,6 +1,16 @@
 <div class="wrap">
 	 <h1>My Learning Plugin</h1>
 	 <?php settings_errors();	  ?>
+
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#tab-1">Mange Settings</a></li>
+		<li><a href="#tab-2">Updates</a></li>
+		<li><a href="#tab-3">About</a></li>
+		
+	</ul>
+
+<div class="tab-content">
+	<div id="tab-1" class="tab-pane active">
 	 <form method="post" action="options.php">
 	 	<?php 
 	 		settings_fields( 'my_learning_plugin_option_group' ); //option_group
@@ -10,4 +20,15 @@
 	 	 ?>
 	 	
 	 </form>
+	</div>
+
+	<div id="tab-2" class="tab-pane">
+		<h3>Update Section</h3>
+	</div>
+
+	<div id="tab-3" class="tab-pane">
+		<h3>About Section</h3>
+	</div>
+
+</div>
 </div>
